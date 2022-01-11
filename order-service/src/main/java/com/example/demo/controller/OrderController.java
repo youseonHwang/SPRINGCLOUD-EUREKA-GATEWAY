cf9807d9bc0d729465d9129de87738ddc52d4644
youseonHwang <dbtjs0221@gmail.com>
2022-01-11 11:17:55 +0900

    RewritePate 필터 추가

--- order-service/src/main/java/com/example/demo/controller/OrderController.java
+++ order-service/src/main/java/com/example/demo/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
 	
-	@GetMapping("/order/info")
+	@GetMapping("/info")
 	public String info(@Value("${server.port}") String port) {
 		return "Order 서비스의 기본 동작 Port: {"+port+"}";
 	}
--- user-service/src/main/java/com/example/demo/controller/UserController.java
+++ user-service/src/main/java/com/example/demo/controller/UserController.java
@@ -2,13 +2,19 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 	
-	@GetMapping("/user/info")
+	@GetMapping("/info")
 	public String info(@Value("${server.port}") String port) {
 		return "User 서비스의 기본 동작 Port: {"+port+"}";
 	}
+	
+	@GetMapping("/auth")
+    public String auth(@RequestHeader(value = "token") String token) {
+        return "token is " + token;
+    }
 }
